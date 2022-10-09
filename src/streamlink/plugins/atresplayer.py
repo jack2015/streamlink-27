f181aae2a67707f81afebc4e1800ce0274c9350d
bastimeyer <mail@bastimeyer.de>
2022-10-10 00:03:31 +0200

    [PATCH] plugins.atresplayer: update input URLs

--- src/streamlink/plugins/atresplayer.py
+++ src/streamlink/plugins/atresplayer.py
@@ -22,10 +22,12 @@ log = logging.getLogger(__name__)
     r"https?://(?:www\.)?atresplayer\.com/"
 ))
 class AtresPlayer(Plugin):
-    def _get_streams(self):
-        self.url = update_scheme("https://", self.url)
-        path = urlparse(self.url).path
+    def __init__(self, *args, **kwargs):
+        super(AtresPlayer, self).__init__(*args, **kwargs)
+        self.url = update_scheme("https://", "{0}/".format(self.url.rstrip('/')))
 
+    def _get_streams(self):
+        path = urlparse(self.url).path
         api_url = self.session.http.get(self.url, schema=validate.Schema(
             re.compile(r"""window.__PRELOADED_STATE__\s*=\s*({.*?});""", re.DOTALL),
             validate.none_or_all(
--- tests/plugins/test_atresplayer.py
+++ tests/plugins/test_atresplayer.py
@@ -1,13 +1,29 @@
+import pytest
+
 from streamlink.plugins.atresplayer import AtresPlayer
 from tests.plugins import PluginCanHandleUrl
+# TODO: from tests.mock import Mock
 
 
 class TestPluginCanHandleUrlAtresPlayer(PluginCanHandleUrl):
     __plugin__ = AtresPlayer
 
     should_match = [
-        'http://www.atresplayer.com/directos/antena3/',
-        'http://www.atresplayer.com/directos/lasexta/',
-        'https://www.atresplayer.com/directos/antena3/',
-        'https://www.atresplayer.com/flooxer/programas/unas/temporada-1/dario-eme-hache-sindy-takanashi-entrevista_123/',
+        "http://www.atresplayer.com/directos/antena3/",
+        "http://www.atresplayer.com/directos/lasexta/",
+        "https://www.atresplayer.com/directos/antena3/",
+        "https://www.atresplayer.com/flooxer/programas/unas/temporada-1/dario-eme-hache-sindy-takanashi-entrevista_123/",
     ]
+
+
+class TestAtresPlayer:
+    @pytest.mark.parametrize("url,expected", [
+        ("http://www.atresplayer.com/directos/antena3", "https://www.atresplayer.com/directos/antena3/"),
+        ("http://www.atresplayer.com/directos/antena3/", "https://www.atresplayer.com/directos/antena3/"),
+        ("https://www.atresplayer.com/directos/antena3", "https://www.atresplayer.com/directos/antena3/"),
+        ("https://www.atresplayer.com/directos/antena3/", "https://www.atresplayer.com/directos/antena3/"),
+    ])
+    def test_url(self, url, expected):
+        # TODO: change to AtresPlayer(Mock(), url)
+        plugin = AtresPlayer(url)
+        assert plugin.url == expected
